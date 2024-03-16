c2942a44d9dc7268396d5bae463a21b581d42638
Purna Shrestha <crush9918@gmail.com>
2024-03-17 00:34:37 +0545

    Add dropdown on the navbar

--- src/components/LearnMore.jsx
+++ src/components/LearnMore.jsx
@@ -1,9 +1,9 @@
 import React from "react";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 
 const LearnMore = () => {
   return (
     <>
-      {" "}
       <button class="group relative rounded-xl p-px text-base font-semibold leading-6 shadow-2xl shadow-luminr-white/[0.1] text-luminr-white mt-10">
         <span class="absolute inset-0 overflow-hidden">
           <span class="absolute inset-0 rounded-xl bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(219,99,50,1)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
@@ -12,23 +12,7 @@ const LearnMore = () => {
           <span className="text-base transition-all duration-300 ease-linear group-hover:text-gradient">
             Learn More
           </span>
-          <span className="inline-block group-hover:translate-x-2 transition duration-200 group-hover:text-luminr-orange">
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              stroke-width="0"
-              viewBox="0 0 16 16"
-              className="mt-0.5"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-              ></path>
-            </svg>
-          </span>
+          <HiOutlineArrowNarrowRight className="text-xl group-hover:translate-x-2 transition duration-200 group-hover:text-luminr-orange" />
         </div>
         <span class="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-[rgb(219,99,50)] to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
       </button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import luminr from "../assets/logo.svg";
 import { navLinks } from "../constants/data";
+import { HiOutlineChevronDown } from "react-icons/hi"; // Importing the dropdown icon
 import Appointment from "./Appointment";
 
 const Navbar = () => {
+  const [showSubMenu, setShowSubMenu] = useState(null);
+
+  const toggleSubMenu = (index) => {
+    setShowSubMenu((prevIndex) => (prevIndex === index ? null : index));
+  };
+
   return (
     <nav className="fixed top-4 w-full z-50">
       <div className="container">
-        {/* <div className="w-full flex items-center justify-between backdrop-blur-md border border-luminr-white/[0.16] px-4 py-2 rounded-full "> */}
-        <div className="flex items-center justify-between text-luminr-white gap-4 mx-auto border border-luminr-white/[0.16] px-4 py-3 rounded-lg backdrop-blur-md z-50">
+        <div className="w-full flex items-center justify-between backdrop-blur-md border border-luminr-white/[0.16] px-4 py-2 rounded-full ">
           <a href="/">
             <img
               src={luminr}
@@ -17,16 +23,37 @@ const Navbar = () => {
             />
           </a>
           <ul className="flex space-x-6">
-            {navLinks.map((link) => (
-              <li key={link.id} className="group relative">
-                <a href={link.url} className="hover:text-gray-400 text-lg">
+            {navLinks.map((link, index) => (
+              <li
+                key={link.id}
+                className="group relative"
+                onMouseEnter={() => toggleSubMenu(index)}
+                onMouseLeave={() => toggleSubMenu(null)}
+              >
+                <a
+                  href={link.url}
+                  className="z-50 text-luminr-white hover:text-white text-lg font-medium flex items-center"
+                >
                   {link.title}
+                  {link.sublinks && <HiOutlineChevronDown className="text-sm ml-1" />}
                 </a>
                 {link.sublinks && (
-                  <ul className="absolute hidden bg-gray-800 text-luminr-white rounded-md py-2 space-y-2 group-hover:block">
+                  <ul
+                    className={`z-40 absolute py-5 transition-all duration-500 ease-out transform origin-top -translate-x-14 ${
+                      showSubMenu === index
+                        ? "opacity-100 scale-100 translate-y-0"
+                        : "opacity-0 scale-0 -translate-y-4"
+                    }`}
+                  >
                     {link.sublinks.map((sublink) => (
-                      <li key={sublink.id}>
-                        <a href={sublink.url} className="hover:text-gray-400">
+                      <li
+                        key={sublink.id}
+                        className="bg-luminr-orange/5 font-bold text-luminr-white hover:bg-gradient hover:text-luminr-blue rounded-full space-y-4 w-48 mb-2 text-center border border-luminr-white/[0.16] backdrop-blur-md"
+                      >
+                        <a
+                          href={sublink.url}
+                          className="inline-block w-full p-4 py-2"
+                        >
                           {sublink.title}
                         </a>
                       </li>
@@ -36,9 +63,6 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
-          {/* <button className="bg-blue-500 hover:bg-blue-700 text-luminr-white font-bold py-2 px-4 rounded">
-            Hop on a call
-          </button> */}
 
           <Appointment />
         </div>
